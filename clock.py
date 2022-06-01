94639a49414aab0a487affc25c30c1dcca4d8c81
LiyangTseng <k00441tsengliyung@gmail.com>
2022-06-01 11:31:01 +0800

    change crawler to OOP, crawl mutiple news simutaneously

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.models import TextSendMessage
 import requests
 from apscheduler.schedulers.blocking import BlockingScheduler
-from crawl_news import crawl
+from crawl_news import CNBC_Crawler
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -13,10 +13,15 @@ line_bot_api = LineBotApi(config.get("line-bot", "channel_access_token"))
 handler = WebhookHandler(config.get("line-bot", "channel_secret"))
 
 
-def Notify_News():
-    title, link = crawl()
+def Notify_News(num_news=3):
+    crawler = CNBC_Crawler()
+    title_link_pairs = crawler.crawl(num_news)
+    msg = ""
+    for title, link in title_link_pairs:
+        msg += title + "\n" + link + "\n\n"
+
     # https://developers.line.biz/en/reference/messaging-api/#send-broadcast-message
-    line_bot_api.broadcast(TextSendMessage(text="{}\n{}".format(title, link)))
+    line_bot_api.broadcast(TextSendMessage(text=msg))
 
 def DoNotSleep():
     url = "https://news-collector-linebot.herokuapp.com/"
@@ -25,6 +30,7 @@ def DoNotSleep():
 sched = BlockingScheduler()
 
 sched.add_job(DoNotSleep, trigger='interval', id="doNotSleeps_job", minutes=20)
-sched.add_job(Notify_News, trigger='cron', id="notify_news_job", hour=10, minute=52)
-sched.add_job(Notify_News, trigger='interval', id="notify_news_job_cont", minutes=5)
+sched.add_job(Notify_News, trigger='cron', id="notify_news_job_once", hour=11, minute=35)
+sched.add_job(Notify_News, trigger='interval', id="notify_news_job_cont_night", day_of_week='mon-fri', hour=21, minute=30)
+sched.add_job(Notify_News, trigger='interval', id="notify_news_job_cont_noon", day_of_week='mon-fri', hour=12, minute=00)
 sched.start()
--- crawl_news.py
+++ crawl_news.py
@@ -1,23 +1,41 @@
 import requests
 from bs4 import BeautifulSoup
 
-def crawl():
-    url = "https://www.cnbc.com/patti-domm/"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, "html.parser")
-    
-    # focus on crawling CNBC week summary
-    targets = soup.find_all("div", class_="RiverCard-standardBreakerCard RiverCard-specialReportsRiver RiverCard-card")
-    for target in targets:
-        # exclude premium content
-        if target.find_all("div", "RiverCard-pro"):
-            continue
-        # get news title and link
-        title = target.find("a", "RiverCard-title").text
-        link = target.find("a", "RiverCard-title").get("href")
-        break
+class Base_Crawler():
+    def __init__(self, web_url):
+        self.response = requests.get(web_url)
+        self.soup = BeautifulSoup(self.response.text, "html.parser")
 
-    return title, link
+    def crawl(self, news_num):
+        pass
+
+class CNBC_Crawler(Base_Crawler):
+    """ crawl week summary (and other news) published by Patti Domm from CNBC """
+    def __init__(self, web_url="https://www.cnbc.com/patti-domm/"):
+        super(CNBC_Crawler, self).__init__(web_url)
+        # observed from the source code of the website
+        self.targets = self.soup.find_all("div", class_="RiverCard-standardBreakerCard RiverCard-specialReportsRiver RiverCard-card")
+
+    def crawl(self, N):
+        """ return lastest n news  """
+        targets = self.soup.find_all("div", class_="RiverCard-standardBreakerCard RiverCard-specialReportsRiver RiverCard-card")
+        self.titles_links = []
+        for target in targets:
+            # exclude premium content
+            if target.find_all("div", "RiverCard-pro"):
+                continue
+            link = target.find("a", "RiverCard-title").get("href")
+            date = link.split("https://www.cnbc.com/")[1][:10]
+            title = "[{date}] {news_title}".format(date=date, 
+                                                    news_title=target.find("a", "RiverCard-title").text)
+            
+            self.titles_links.append((title, link))
+            
+        return self.titles_links[:N]
 
 if __name__ == "__main__":
-    crawl()
+    c = CNBC_Crawler()
+    title_link_pairs = c.crawl(3)
+    for title, link in title_link_pairs:
+        print(title)
+        print(link)
